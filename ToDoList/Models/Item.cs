9375003b28b779e0c68c31f2ce11fb51401a7ab5
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-31 14:47:29 -0700

    Using many-to-many relationships.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,32 +1,16 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
   public class Item
   {
+    public int ItemId { get; set; }
+    [Required(ErrorMessage = "The item's description can't be empty!")]
     public string Description { get; set; }
-    public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
-
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
-    }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<ItemTag> JoinEntities { get;}
   }
 }
